e9abb6bd8ddff9c779ea848a6c4f3ec589dcbe43
Asify12 <107687789+Asify12@users.noreply.github.com>
2022-09-24 21:32:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -24,7 +24,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001608251675"))
 
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ʏᴏʀ ғᴏʀɢᴇʀ ᴍᴜsɪᴄ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "ᴍɪᴋᴜ ɴᴀᴋᴀɴᴏ ᴍᴜsɪᴄ")
 
 OWNER_ID = list(
     map(int, getenv("OWNER_ID", "5667156680").split())
