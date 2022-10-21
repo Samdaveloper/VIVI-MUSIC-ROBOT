488e93540c563e37355baaa6203e01bf6a0417fe
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-21 17:04:14 +0530

    Update start.py

--- VIVI/plugins/Robot/start.py
+++ VIVI/plugins/Robot/start.py
@@ -187,7 +187,6 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAx0CXoHSygACLgFjG04iWOIPflIQ7w5Ts9wjzqZAcwACvgYAArmZ2VRdl4hkHpDanR4E")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
